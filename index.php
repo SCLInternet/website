2d13b8d2a0be68bc94f542175d53392162e1d914
Dan Conway <dan@scl.co.uk>
2015-09-28 14:58:00 +0100

    quick draft for temp scl landing pagein html and css

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+	<div id="page" class="hfeed site">
+<!-- 		<a class="skip-link screen-reader-text" href="#content">
+			Skip to content
+		</a>
+		 -->
+		<div class="top-bar">
+			<header id="masthead" class="site-header" role="banner">
+				<h1>SCL <span>Internet Services</span></h1>
+			</header>
+		</div>
+
+		<div id="content" class="site-content">
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<div class="banner">
+				<h2 class="tel">t:01239 622 411<br>e: <a href="mailto:info@scl.co.uk">info@scl.co.uk</a></h2>
+					<div class="banner-content">
+						<h2>Welcome to SCL Internet Services</h2>
+						<p>We are currently upgrading our site along with all services. See below for contact details and a list of our services.</p>
+					</div>
+				</div>
+				<div class="break"></div>
+				<div class="main-content">
+					<div class="how-to">
+							<div class="title">
+								<h2>How to get in touch</h2>
+							</div>
+							<div class="how-to-content">
+							<h3>By email - </h3>
+							<p><strong>General Enquiries</strong> - <a href="mailtoinfo:@scl.co.uk">info@scl.co.uk</a></p><p>
+							<strong>Sales &amp; Billing</strong> - <a href="mailto:billing@scl.co.uk">billing@scl.co.uk</a></p><p>
+							<strong>Support</strong> - <a href="mailto:support@scl.co.uk">support@scl.co.uk</a></p>
+<br>
+							<h3>By phone - </h3>
+							<p>01239 622 411<br>(open from 9am - 5pm gmt+/- 0)</p>
+							
+<br>
+							<h3>By mail - </h3>
+							<p>SCL Internet<br>PO Box 8<br>Cardigan<br>SA41 3YA<br>Wales</p>
+						</div>
+					</div>
+					<div class="what-we-offer">
+							<div class="title">
+								<h2>What we offer</h2>
+							</div>
+							<div class="what-we-do-content">
+								<div class="hosting service">
+									<h2 class="service-title">Hosting</h2>
+									<h3 class="price"><span>from:</span> £40pa</h3>
+									<p class="description">A wide range of professional packages to suite any requirements.</p>
+								</div>
+								<div class="domains service">
+									<h2 class="service-title">Domains</h2>
+									<h3 class="price"><span>from:</span> £4.99pa</h3>
+									<p class="description">Register your domain to secure your identity on the web.</p>
+								</div>
+								<div class="web-design service">
+									<h2 class="service-title">Web Design</h2>
+									<h3 class="price"><span>from:</span> £250</h3>
+									<p class="description">You name it, we make it. From simple web pages to fully functional e-commerce CMS websites. All fully responsive.</p>
+								</div>
+								<div class="development service">
+									<h2 class="service-title">Development</h2>
+									<p class="description">PHP &amp; My SQL, Wordpress, Magento, iPhone &amp; Android apps, custom software &amp; more</p>
+								</div>
+							</div>
+					</div>
+				</div>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="footer-content">
+			<span class="sep">&copy; SCL <?php echo date('Y') ?></span>
+		</div>
+	</footer>
+			</main>
+		</div>
+
+	</div>
+
+
+
+</body>
+</html>
